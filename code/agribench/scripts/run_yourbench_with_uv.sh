955adf4592ddf6777ac444d0d8c72d6832dcc250
elenka_bugaiova <java.elena@gmail.com>
2026-02-12 11:28:19 +0100

    fixed EOL settings

--- code/agribench/scripts/run_yourbench_with_uv.sh
+++ code/agribench/scripts/run_yourbench_with_uv.sh
@@ -1,1 +1,6 @@
-pip install uvuv venv --python 3.12source .venv/bin/activateuv pip install yourbenchyourbench run ../config.yaml
+pip install uv
+uv venv --python 3.12
+source .venv/bin/activate
+
+uv pip install yourbench
+yourbench run ../config.yaml
